6ea63897282e18d3f90e1ed07f9a01f237640a28
NatsuX <ketutdiar6@gmail.com>
2021-09-24 12:51:00 +0800

    Send Email to Developer when Fatal Error occurs

--- src/Support/mail sender/lib/mail.js
+++ src/Support/mail sender/lib/mail.js
@@ -6,20 +6,20 @@ class HatsuMailer {
         this.transport = nodemailer.createTransport({
             host: process.env.MAIL_SERVER,
             port: process.env.MAIL_PORT,
-            secure: false, // true for 465, false for other ports
+            secure: false, //If the Server use port 465 set it to true otherwise set it to false
             auth: {
-                user: process.env.MAIL_USER, // generated ethereal user
-                pass: process.env.MAIL_PASSWORD, // generated ethereal password
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASSWORD,
             },
         });
     }
     async sendError(errorMessage, Guild, Date) {
         let msg = await this.transport.sendMail({
             from: `Hatsu | ${version} <${process.env.MAIL_USER}>`,
-            to: "ketutdiar6@gmail.com",
+            to: process.env.DEV_EMAIL,
             subject: `Hatsu Got Fatal Error on ${Guild.id},Please Read this!`,
-            text: "**Hi!,I Got Fatal Error on one of my system, i already run save procedure so this error will not get\n" +
-                "more worse than before, please read the error below this message!**\n" + "\n" +
+            text: "Hi!,I Got Fatal Error on one of my system, i already run save procedure so this error will not get\n" +
+                "more worse than before, please read the error below this message!\n" + "\n" +
                 `Date:${Date}\n` + `Guild Name:${Guild.name}\n` + `Guild ID:${Guild.id}\n` + `Hatsu Version:${version}\n` + "\n" + `Error Message:\n ${errorMessage}`
         });
         console.log("Message Send! " + `${msg.messageId}`);
